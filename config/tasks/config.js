6497a043b0873268774fa3f9762b548798968399
rongliu <570899307@qq.com>
2015-12-27 15:38:11 +0800

    modify the image gulp

--- config/tasks/config.js
+++ config/tasks/config.js
@@ -18,6 +18,6 @@ module.exports = {
     path: 'public/',
     javascriptPath: 'assets/javascript',
     stylesheetsPath: 'assets/stylesheets',
-    imagePath: 'assets/images',
+    imagePath: 'assets/',
   }
 };
