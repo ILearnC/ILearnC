ab8fe44025a37c2c2e07de56fd9ecc0d707d8687
rongliu <570899307@qq.com>
2016-01-12 15:04:27 +0800

    fix the user name position

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render 'login'
+      log_in @user
+      has_user
+      redirect_to :controller => 'index', :action => 'index'
     else
       render 'register'
     end
