b1d77f59aead07a95699f8952453278b359b0ffe
rongliu <570899307@qq.com>
2016-01-24 20:55:42 +0800

    remove the change the skill type migrate

--- db/migrate/20160124104820_change_the_skill_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTheSkillType < ActiveRecord::Migration
-  def up
-    change_column :skills, :skill_type, :integer
-  end
-
-  def down
-    change_column :skills, :skill_type, :integer
-  end
-end
