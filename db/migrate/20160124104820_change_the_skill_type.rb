ef51571b4cf21352651ac53c3de4258d80b0a5d3
rongliu <570899307@qq.com>
2016-01-24 19:05:30 +0800

    change the skill type

---
+++ db/migrate/20160124104820_change_the_skill_type.rb
@@ -0,0 +1,9 @@
+class ChangeTheSkillType < ActiveRecord::Migration
+  def up
+    change_column :skills, :skill_type, :integer
+  end
+
+  def down
+    change_column :skills, :skill_type, :integer
+  end
+end
