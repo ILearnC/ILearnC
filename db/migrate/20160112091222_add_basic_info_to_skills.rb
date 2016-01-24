d0766697f0c2adf9ed876ffffd6ee2651c0bccc7
rongliu <570899307@qq.com>
2016-01-24 20:55:42 +0800

    remove the change the skill type migrate

--- db/migrate/20160112091222_add_basic_info_to_skills.rb
+++ db/migrate/20160112091222_add_basic_info_to_skills.rb
@@ -1,7 +1,7 @@
 class AddBasicInfoToSkills < ActiveRecord::Migration
   def change
     add_column :skills, :teacher, :string
-    add_column :skills, :skill_type, :string
+    add_column :skills, :skill_type, :integer
     add_column :skills, :start_date, :date
     add_column :skills, :end_date, :date
     add_column :skills, :head_count, :integer
