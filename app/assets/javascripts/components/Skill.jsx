a89d1a29b315d6c8a43f9d9f869fdba4d59b6709
rongliu <570899307@qq.com>
2016-01-29 14:58:00 +0800

    modify the skill component style

--- app/assets/javascripts/components/Skill.jsx
+++ app/assets/javascripts/components/Skill.jsx
@@ -16,11 +16,10 @@ let Skill = React.createClass({
             <div className='item-description'>{this.props.skill.description}</div>
           </div>
           <div>
-            <div>{this.props.skill.title}</div>
-            <div>上课老师：{this.props.skill.teacher}</div>
+            <div><h4>{this.props.skill.title}</h4>{this.props.skill.teacher}</div>
             <div>开课时间：{this.props.skill.start_date}</div>
             <div>结课时间：{this.props.skill.end_date}</div>
-            <div>班级人数：{this.props.skill.head_count}</div>
+            <div>班级人数：{this.props.skill.head_count}/人</div>
           </div>
         </div>
       </div>
