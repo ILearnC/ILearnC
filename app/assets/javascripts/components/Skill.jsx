e97a097795bff41fb51a61a67d2e0943d02cbc78
rongliu <570899307@qq.com>
2016-01-29 14:19:00 +0800

    add hover style and can show the description

--- app/assets/javascripts/components/Skill.jsx
+++ app/assets/javascripts/components/Skill.jsx
@@ -4,13 +4,23 @@ let Skill = React.createClass({
   },
 
   render: function () {
+    styleItem = {
+      backgroundImage: 'url('+'/assets/images/default.png'+')',
+      height: 200,
+      backgroundSize: 'cover'
+    }
     return (
       <div className='ilearnc-layout'>
         <div className='ilearnc-layout--item'>
-          <img src='/assets/images/default.png' className='img-rounded ilearnc-thumbnail'/>
+          <div className='img-rounded item-image' style={styleItem}>
+            <div className='item-description'>{this.props.skill.description}</div>
+          </div>
           <div>
             <div>{this.props.skill.title}</div>
-            <div>{this.props.skill.description}</div>
+            <div>上课老师：{this.props.skill.teacher}</div>
+            <div>开课时间：{this.props.skill.start_date}</div>
+            <div>结课时间：{this.props.skill.end_date}</div>
+            <div>班级人数：{this.props.skill.head_count}</div>
           </div>
         </div>
       </div>
