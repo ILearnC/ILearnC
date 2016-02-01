498521e24bc576aec30f2f6fe1a7faa74b3c1c4f
rongliu <570899307@qq.com>
2016-02-01 11:20:19 +0800

    modifty the add course page style

--- app/assets/javascripts/course/AddCourse.jsx
+++ app/assets/javascripts/course/AddCourse.jsx
@@ -21,27 +21,29 @@ let AddCourse = React.createClass({
     })
 
     return (
-      <div className='register'>
-        <h1>添加课程</h1>
-        <form action='/addcourse' method='post'>
-          <div>
-            <input id='titel' className='ilearn__input' name='skill[title]' placeholder='请输入课程名称' type='text' />
-          </div>
-          <div>
-            <input id='teacher' className='ilearn__input' name='skill[teacher]' placeholder='请输入授课教师' type='text' />
-          </div>
-          <select value={this.state.selectskill} onChange={this._setSkillType} name='skill[skill_type]'>
+      <div className='ilearnc-body'>
+        <form action='/addcourse' method='post' className='col-md-6'>
+          <h1>添加课程</h1>
+          <input id='titel' className='form-control ilearn__input'
+            name='skill[title]' placeholder='请输入课程名称' type='text' />
+
+          <input id='teacher' className='form-control ilearn__input'
+            name='skill[teacher]' placeholder='请输入授课教师' type='text' />
+
+          <select className='form-control ilearn__input'
+            value={this.state.selectskill} onChange={this._setSkillType} name='skill[skill_type]'>
             {skillList}
           </select>
+
           <Datepicker message='请输入课程开始时间 2014-01-12' name='skill[start_date]' datepickerId='start_date'/>
+
           <Datepicker message='请输入课程结束时间 2015-01-22' name='skill[end_date]' datepickerId='end_date'/>
-          <div>
-            <input id='head_count' className='ilearn__input' name='skill[head_count]' placeholder='请输入课程上线人数' type='text'/>
-          </div>
-          <div>
-            <input id='description' className='ilearn__input' name='skill[description]' placeholder='请输入课程描述' type='text'/>
-          </div>
-          <input className='ilearn__button' name='addcourse' type='submit' value='添加'/>
+          <input id='head_count' className='form-control ilearn__input'
+            name='skill[head_count]' placeholder='请输入课程上线人数' type='text'/>
+
+          <textarea rows='3' id='description' className='form-control ilearn__input'
+            name='skill[description]' placeholder='请输入课程描述' type='text'/>
+          <input className='btn btn-primary btn-block' name='addcourse' type='submit' value='添加'/>
         </form>
       </div>
     )
