258493c6842e8709f0bc1313363d0338fd0b6a15
rongliu <570899307@qq.com>
2016-02-01 11:19:56 +0800

    modify the datepicker style

--- app/assets/javascripts/components/Datepicker.jsx
+++ app/assets/javascripts/components/Datepicker.jsx
@@ -13,19 +13,13 @@ let Datepicker = React.createClass({
 
   render: function () {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm-6'>
-            <div className='form-group'>
-              <div className='input-group date' id={this.props.datepickerId} onClick={this._setDatepicker}>
-                <input type='text' className='form-control'
-                  placeholder={this.props.message} name={this.props.name}/>
-                <span className='input-group-addon'>
-                  <span className='glyphicon glyphicon-calendar'></span>
-                </span>
-              </div>
-            </div>
-          </div>
+      <div className='form-group'>
+        <div className='input-group date' id={this.props.datepickerId} onClick={this._setDatepicker}>
+          <input type='text' className='form-control'
+            placeholder={this.props.message} name={this.props.name}/>
+          <span className='input-group-addon'>
+            <span className='glyphicon glyphicon-calendar'></span>
+          </span>
         </div>
       </div>
     )
